15ad96388798cdaadb92a34f6008244b1bcfe2a8
fengxxc <fengxxc@gmail.com>
2023-11-20 15:45:26 +0800

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='vocal-remover',
+    version='0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'tqdm>=4.30',
+        'librosa>=0.9',
+        'opencv_python>=4.2.0',
+        'resampy>=0.4.2',
+    ],
+    entry_points={
+        'console_scripts': [
+            # 'your_command = your_package_name.module:main_function',
+        ],
+    },
+    author='fengxxc',
+    description='Vocal Remover using Deep Neural Networks',
+    license='MIT',
+)

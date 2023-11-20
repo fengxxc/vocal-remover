0be1e9b229b0de3813652baf761edb4b8e8e3845
fengxxc <fengxxc@gmail.com>
2023-11-20 17:57:30 +0800

    Revert "chore(setup): change name and packages"
    This reverts commit 6f4f301e40c01a313237855c44cd89d0c9d211f9.

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-    name='vocal_remover',
+    name='vocal-remover',
     version='0.1.1',
-    packages=["vocal_remover"],
+    packages=find_packages(),
     install_requires=[
         'tqdm>=4.30',
         'librosa>=0.9',
         'opencv_python>=4.2.0',
         'resampy>=0.4.2',
     ],
-    # entry_points={
-    #     'console_scripts': [
-    #         'your_command = your_package_name.module:main_function',
-    #     ],
-    # },
+    entry_points={
+        'console_scripts': [
+            # 'your_command = your_package_name.module:main_function',
+        ],
+    },
     author='fengxxc',
     description='Vocal Remover using Deep Neural Networks',
     license='MIT',

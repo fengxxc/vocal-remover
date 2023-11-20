6f4f301e40c01a313237855c44cd89d0c9d211f9
fengxxc <fengxxc@gmail.com>
2023-11-20 17:46:40 +0800

    chore(setup): change name and packages

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
-    name='vocal-remover',
+    name='vocal_remover',
     version='0.1.1',
-    packages=find_packages(),
+    packages=["vocal_remover"],
     install_requires=[
         'tqdm>=4.30',
         'librosa>=0.9',
         'opencv_python>=4.2.0',
         'resampy>=0.4.2',
     ],
-    entry_points={
-        'console_scripts': [
-            # 'your_command = your_package_name.module:main_function',
-        ],
-    },
+    # entry_points={
+    #     'console_scripts': [
+    #         'your_command = your_package_name.module:main_function',
+    #     ],
+    # },
     author='fengxxc',
     description='Vocal Remover using Deep Neural Networks',
     license='MIT',
